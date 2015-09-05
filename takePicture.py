9ab1e9b91693f0a47b0a089da56ce8d88ba81ccf
Zack Scholl <zack.scholl@gmail.com>
2015-09-05 08:54:10 -0400

    Made takePicture

--- takePicture.py
+++ takePicture.py
@@ -6,7 +6,7 @@ def takePicture():
 	with picamera.PiCamera() as camera:
 		camera.resolution = (640, 480)
 		camera.vflip = True
-		time.sleep(5)
+		time.sleep(0.1)
 		camera.capture('foo.jpg')
 
 # sshfs phi@192.168.1.22:/www/oliver_tv/stream mount
@@ -14,6 +14,4 @@ while True:
 	print('picture')
 	takePicture()
 	os.system('mv foo.jpg ./static/image_stream.jpg')
-	#os.system('raspistill -w 640 -h 480 -q 50 -o ./mount/image_stream.jpg -rot 180 -th 0:0:0')
-	#os.system('rsync -avr ./stream/image_stream.jpg phi@192.168.1.22:/www/oliver_tv/stream/')
 
